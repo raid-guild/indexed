99196095a760bbe05904316c15927ef96f889326
Aaron <cryptotrees@gmail.com>
2021-01-28 00:44:56 -0500

    Update contribute link in footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ module.exports = {
       //   src: '/img/kevin.png',
       // },
 
-      copyright: `indexed.wtf is a collectively built hub for indexing scaling solutions for Ethereum. Everyone is welcome to collaborate, currently via the <a href="/docs/contribute-doc/" target="#">docs</a>.
+      copyright: `indexed.wtf is a collectively built hub for indexing scaling solutions for Ethereum. Everyone is welcome to collaborate, currently via this <a href="https://docs.google.com/forms/d/e/1FAIpQLScbMNeGoQEOrvuuvrk6xaZvdE2PzKiALAMcZ14v48ARS4fIHg/viewform" target="#">form</a>.
       </br>
       Bootstrapped by <a class="ow-link" href="https://twitter.com/owocki" target="#">Owocki</a> via <a class="rg-link" href="https://raidguild.org" target="#">raidguild.org</a>
       `,
@@ -43,8 +43,6 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/raid-guild/indexed/blob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
