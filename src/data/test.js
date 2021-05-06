6990a86aecab55b76c63734e21bd8741c57721a3
Aaron <cryptotrees@gmail.com>
2021-05-06 14:19:19 -0400

    Update everest logo on homepage and docs

--- src/data/test.js
+++ src/data/test.js
@@ -1,82 +1,15 @@
 const testData = [
 	{
 		featured: true,
-		title: 'Polygon',
+		title: 'Everest',
 		body:
-			'Matic Network brings massive scale to Ethereum using an adapted version of Plasma with PoS based side chains.',
+			'Global Banking Platform',
 		bullets: [
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+			
 		],
-		url: 'polygon-doc',
-		logoPath: '/img/matic.svg',
-	},
-	{
-		featured: true,
-		title: 'xDai Network',
-		body:
-			'The xDai Chain is a stable payments blockchain designed for fast and inexpensive stable transactions. xDai is used for transactions, payments and fees.',
-		bullets: [
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-		],
-		url: 'xdai-doc',
-		logoPath: '/img/xdai_alternative.svg',
-	},
-	{
-		featured: false,
-		title: 'KChannels',
-		body:
-			'Kchannels is a new payment channel platform for Ethereum. It is non-custodial and trust-minimized, and its primary focus is on great UX.',
-		bullets: [
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-		],
-		url: 'kchannels-doc',
-		logoPath: '/img/Kchannel.png',
-	},
-	{
-		featured: true,
-		title: 'Polygon',
-		body:
-			'Matic Network brings massive scale to Ethereum using an adapted version of Plasma with PoS based side chains.',
-		bullets: [
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-		],
-		url: 'polygon-doc2',
-		logoPath: '/img/matic.svg',
-	},
-	{
-		featured: true,
-		title: 'xDai Network',
-		body:
-			'The xDai Chain is a stable payments blockchain designed for fast and inexpensive stable transactions. xDai is used for transactions, payments and fees.',
-		bullets: [
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-		],
-		url: 'xdai-doc3',
-		logoPath: '/img/xdai_alternative.svg',
-	},
-	{
-		featured: false,
-		title: 'KChannels',
-		body:
-			'Kchannels is a new payment channel platform for Ethereum. It is non-custodial and trust-minimized, and its primary focus is on great UX.',
-		bullets: [
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-			'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-		],
-		url: 'kchannels-doc4',
-		logoPath: '/img/Kchannel.png',
-	},
+		url: 'everest-doc',
+		logoPath: '/img/everest.png',
+	}
 ]
 
 export default testData
