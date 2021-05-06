3e787ab266a05de4dc2f75b32ae7f0a6d63d3dd1
Aaron <cryptotrees@gmail.com>
2021-05-06 14:56:01 -0400

    Update meter homepage logo and docs

--- src/data/test.js
+++ src/data/test.js
@@ -5,11 +5,20 @@ const testData = [
 		body:
 			'Global Banking Platform',
 		bullets: [
-			
 		],
 		url: 'everest-doc',
 		logoPath: '/img/everest.png',
-	}
+	},
+	{
+		featured: true,
+		title: 'Meter',
+		body:
+			'Meter is the most decentralized and fastest Ethereum sidechain network with a native metastable gas currency. It connects Ethereum with other public chians as a layer 2 protocol.',
+		bullets: [
+		],
+		url: 'meter-doc',
+		logoPath: '/img/meterpng.png',
+	},
 ]
 
 export default testData
